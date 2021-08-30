dba1fbc964cd416b5e714e40c32dfe12ecc23882
CodeMastah <paul@project-x.site>
2021-08-30 11:44:00 -0400

    Updated bib-input component

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,120 +1,136 @@
 <template>
   <div class="wrapper">
     <div class="d-flex justify-center pt-1">
-      <img src="/img/logo_vertical.png" class="wrapper__logo">
+      <img src="/img/logo_vertical.png" class="wrapper__logo" />
     </div>
     <div class="pt-2">
       <bib-tabs :tabs="tabs" @change="changeTab" :value="activeTab"></bib-tabs>
     </div>
-     <div class="wrapper__form" v-if="activeTab == 'Sign in'">
-        <bib-form-group required label="Email ">
-            <template v-slot:input>
-                <bib-input :variant="emailVariant" :value="userInfo.email" @change="setEmail" placeholder="Enter email..."></bib-input>
-            </template>
-        </bib-form-group>
+    <div class="wrapper__form" v-if="activeTab == 'Sign in'">
+      <bib-form-group required label="Email ">
+        <template v-slot:content>
+          <bib-input
+            :variant="emailVariant"
+            :value="userInfo.email"
+            @change="setEmail"
+            placeholder="Enter email..."
+          ></bib-input>
+        </template>
+      </bib-form-group>
 
-        <bib-form-group required class="pt-1" label="Password ">
-            <template v-slot:input>
-                <bib-input :variant="passwordVariant" :value="userInfo.password" @change="setPassword" type="password" placeholder="*********"></bib-input>
-            </template>
-        </bib-form-group>
-        <div class="d-flex justify-end">
-            <small><bib-link label="Forgot Password?" class="text-bold text-primary"></bib-link></small>
-        </div>
-        <div class="wrapper__form__submit">
-            <bib-button :disabled="isDisabled" @click.native="loginWith" label="Sign in" class="text-center" size="md" :variant="submitVariant"></bib-button>
-        </div>
+      <bib-form-group required class="pt-1" label="Password ">
+        <template v-slot:content>
+          <bib-input
+            :variant="passwordVariant"
+            :value="userInfo.password"
+            @change="setPassword"
+            type="password"
+            placeholder="*********"
+          ></bib-input>
+        </template>
+      </bib-form-group>
+      <div class="d-flex justify-end">
+        <small
+          ><bib-link
+            label="Forgot Password?"
+            class="text-bold text-primary"
+          ></bib-link
+        ></small>
+      </div>
+      <div class="wrapper__form__submit">
+        <bib-button
+          :disabled="isDisabled"
+          @click.native="loginWith"
+          label="Sign in"
+          class="text-center"
+          size="md"
+          :variant="submitVariant"
+        ></bib-button>
+      </div>
     </div>
-
   </div>
 </template>
 <script>
-import { email, required, minLength } from 'vuelidate/lib/validators'
+import { email, required, minLength } from "vuelidate/lib/validators";
 
 export default {
   data() {
     return {
       activeTab: "Sign in",
-      userInfo: {email: "jest-user@biztree.com", password: "password123"},
-      emailVariant: '',
-      submitVariant: 'secondary',
-      passwordVariant: '',
+      userInfo: { email: "jest-user@biztree.com", password: "password123" },
+      emailVariant: "",
+      submitVariant: "secondary",
+      passwordVariant: "",
       isDisabled: false,
-      tabs: [
-        "Register",
-        "Sign in"
-      ],
+      tabs: ["Register", "Sign in"]
     };
   },
   validations: {
     userInfo: {
       email: { required, email },
-      password: { required, minLength: minLength(6) },
+      password: { required, minLength: minLength(6) }
     }
   },
-  created(){
-    this.$v.$touch()
+  created() {
+    this.$v.$touch();
     if (this.$v.$pending || this.$v.$error) {
-      this.submitVariant = 'secondary'
-      this.isDisabled = true
-    }else{
-      this.submitVariant = 'success'
+      this.submitVariant = "secondary";
+      this.isDisabled = true;
+    } else {
+      this.submitVariant = "success";
     }
   },
   methods: {
     changeTab(value) {
       this.activeTab = value;
-      if(value == 'Register'){
-         window.location.href = '/auth/register';
+      if (value == "Register") {
+        window.location.href = "/auth/register";
       }
     },
     setEmail(event) {
-        this.userInfo.email = event.target.value
-        this.$v.$touch()
+      this.userInfo.email = event.target.value;
+      this.$v.$touch();
 
-        if (this.$v.$pending || this.$v.$error) {
-            this.submitStatus = 'ERROR'
-            this.emailVariant = "danger"
-            this.submitVariant = "secondary"
-            this.isDisabled = true
-        } else {
-          this.submitStatus = 'OK'
-          this.emailVariant = ""
-          this.submitVariant = "success"
-          this.isDisabled = false
+      if (this.$v.$pending || this.$v.$error) {
+        this.submitStatus = "ERROR";
+        this.emailVariant = "danger";
+        this.submitVariant = "secondary";
+        this.isDisabled = true;
+      } else {
+        this.submitStatus = "OK";
+        this.emailVariant = "";
+        this.submitVariant = "success";
+        this.isDisabled = false;
       }
-
     },
     setPassword(event) {
-        this.userInfo.password = event.target.value
-        this.$v.$touch()
+      this.userInfo.password = event.target.value;
+      this.$v.$touch();
 
-        if (this.$v.$pending || this.$v.$error) {
-            this.submitStatus = 'ERROR'
-            this.passwordVariant = "danger"
-            this.submitVariant = "secondary"
-            this.isDisabled = true
-
-        } else {
-          this.submitStatus = 'OK'
-          this.passwordVariant = ""
-          this.submitVariant = "success"
-          this.isDisabled = false
-          
+      if (this.$v.$pending || this.$v.$error) {
+        this.submitStatus = "ERROR";
+        this.passwordVariant = "danger";
+        this.submitVariant = "secondary";
+        this.isDisabled = true;
+      } else {
+        this.submitStatus = "OK";
+        this.passwordVariant = "";
+        this.submitVariant = "success";
+        this.isDisabled = false;
+      }
+    },
+    async loginWith() {
+      try {
+        let response = await this.$auth.loginWith("local", {
+          data: this.userInfo
+        });
+      } catch (err) {
+        console.log(err);
       }
 
-    },
-    async loginWith(){
-        try {
-        let response = await this.$auth.loginWith('local', { data: this.userInfo })
-        } catch (err) {
-            console.log(err)
-        }
-
-        console.log(this.$auth.loggedIn)
+      console.log(this.$auth.loggedIn);
     }
-  },
-  
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -123,18 +139,18 @@ export default {
   margin: 6.5rem auto;
   padding: 1.5rem;
   border: 1px solid $gray4;
-  &__logo{
-      width: 11rem;
+  &__logo {
+    width: 11rem;
   }
-  &__form{
-      border-top: 1px solid $gray4;
-      padding-top: 1.5rem;
+  &__form {
+    border-top: 1px solid $gray4;
+    padding-top: 1.5rem;
 
-    &__submit{
-        padding-top: 1.5rem;
-         .button{
-            width: 100%;
-        }
+    &__submit {
+      padding-top: 1.5rem;
+      .button {
+        width: 100%;
+      }
     }
   }
 }
--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,94 +1,152 @@
 <template>
   <div class="wrapper">
-   <div class="d-flex justify-center pt-1">
-      <img src="/img/logo_vertical.png" class="wrapper__logo">
+    <div class="d-flex justify-center pt-1">
+      <img src="/img/logo_vertical.png" class="wrapper__logo" />
     </div>
     <div class="pt-2">
       <bib-tabs :tabs="tabs" @change="changeTab" :value="activeTab"></bib-tabs>
     </div>
     <div class="wrapper__form" v-if="activeTab == 'Register'">
+      <bib-form-group required label="Email Address ">
+        <template v-slot:content>
+          <bib-input
+            :value="userInfo.email"
+            @change="
+              event => {
+                userInfo.email = event.target.value;
+              }
+            "
+            placeholder="Enter email..."
+          ></bib-input>
+        </template>
+      </bib-form-group>
 
-        <bib-form-group required label="Email Address ">
-            <template v-slot:input>
-                <bib-input :value="userInfo.email" @change="(event) => { userInfo.email = event.target.value }" placeholder="Enter email..."></bib-input>
-            </template>
-        </bib-form-group>
+      <bib-form-group class="pt-05" label="First Name ">
+        <template v-slot:content>
+          <bib-input
+            :value="userInfo.fname"
+            @change="
+              event => {
+                userInfo.fname = event.target.value;
+              }
+            "
+            placeholder="Enter email..."
+          ></bib-input>
+        </template>
+      </bib-form-group>
 
-        <bib-form-group class="pt-05" label="First Name ">
-            <template v-slot:input>
-                <bib-input :value="userInfo.fname" @change="(event) => { userInfo.fname = event.target.value }" placeholder="Enter email..."></bib-input>
-            </template>
-        </bib-form-group>
-        
-        <bib-form-group class="pt-05" label="Last Name ">
-            <template v-slot:input>
-                <bib-input :value="userInfo.lname" @change="(event) => { userInfo.lname = event.target.value }" placeholder="Enter email..."></bib-input>
-            </template>
-        </bib-form-group>
+      <bib-form-group class="pt-05" label="Last Name ">
+        <template v-slot:content>
+          <bib-input
+            :value="userInfo.lname"
+            @change="
+              event => {
+                userInfo.lname = event.target.value;
+              }
+            "
+            placeholder="Enter email..."
+          ></bib-input>
+        </template>
+      </bib-form-group>
 
-        <bib-form-group class="pt-05" label="Telephone Number ">
-            <template v-slot:input>
-                <bib-input :value="userInfo.phone" @change="(event) => { userInfo.phone = event.target.value }" placeholder="Enter email..."></bib-input>
-            </template>
-        </bib-form-group>
+      <bib-form-group class="pt-05" label="Telephone Number ">
+        <template v-slot:content>
+          <bib-input
+            :value="userInfo.phone"
+            @change="
+              event => {
+                userInfo.phone = event.target.value;
+              }
+            "
+            placeholder="Enter email..."
+          ></bib-input>
+        </template>
+      </bib-form-group>
 
-        <bib-form-group required class="pt-05" label="Password ">
-            <template v-slot:input>
-                <bib-input :value="userInfo.password" @change="(event) => { userInfo.password = event.target.value }" type="password" placeholder="*********"></bib-input>
-            </template>
-        </bib-form-group>
-        
-         <bib-form-group required class="pt-05" label="Confirm Password ">
-            <template v-slot:input>
-                <bib-input :value="userInfo.confirmPassword" @change="(event) => { userInfo.confirmPassword = event.target.value }" type="password" placeholder="*********"></bib-input>
-            </template>
-        </bib-form-group>
-      
-        <div class="wrapper__form__submit">
-            <bib-button @click.native="register" label="Register" class="text-center" size="md" variant="success"></bib-button>
-        </div>
-        <div class="text-gray6 pt-1">
-            <small>By selecting Register, you agree to our <a href="#">Terms of Use</a> and have read and acknowledge our <a href="#">Privacy Policy</a>.</small>
-        </div>
+      <bib-form-group required class="pt-05" label="Password ">
+        <template v-slot:content>
+          <bib-input
+            :value="userInfo.password"
+            @change="
+              event => {
+                userInfo.password = event.target.value;
+              }
+            "
+            type="password"
+            placeholder="*********"
+          ></bib-input>
+        </template>
+      </bib-form-group>
+
+      <bib-form-group required class="pt-05" label="Confirm Password ">
+        <template v-slot:content>
+          <bib-input
+            :value="userInfo.confirmPassword"
+            @change="
+              event => {
+                userInfo.confirmPassword = event.target.value;
+              }
+            "
+            type="password"
+            placeholder="*********"
+          ></bib-input>
+        </template>
+      </bib-form-group>
+
+      <div class="wrapper__form__submit">
+        <bib-button
+          @click.native="register"
+          label="Register"
+          class="text-center"
+          size="md"
+          variant="success"
+        ></bib-button>
+      </div>
+      <div class="text-gray6 pt-1">
+        <small
+          >By selecting Register, you agree to our
+          <a href="#">Terms of Use</a> and have read and acknowledge our
+          <a href="#">Privacy Policy</a>.</small
+        >
+      </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-    auth: false,
+  auth: false,
   methods: {
     changeTab(value) {
       this.activeTab = value;
-      if(value == 'Sign in'){
-         window.location.href = '/auth/login';
+      if (value == "Sign in") {
+        window.location.href = "/auth/login";
       }
     },
-     async register(){
-        try {
-        let response = await this.$auth.loginWith('local', { data: this.userInfo })
-            // console.log(response)
-        } catch (err) {
-            console.log(err)
-        }
+    async register() {
+      try {
+        let response = await this.$auth.loginWith("local", {
+          data: this.userInfo
+        });
+        // console.log(response)
+      } catch (err) {
+        console.log(err);
+      }
     }
   },
   data() {
     return {
       activeTab: "Register",
-      userInfo:{
-          email: '',
-          password: '',
-          fname: '',
-          lname: '',
-          phone: '',
-          confirmPassword: ''
+      userInfo: {
+        email: "",
+        password: "",
+        fname: "",
+        lname: "",
+        phone: "",
+        confirmPassword: ""
       },
-      tabs: [
-        "Register",
-        "Sign in"
-      ],
+      tabs: ["Register", "Sign in"]
     };
-  },
+  }
 };
 </script>
 <style lang="scss" scoped>
@@ -98,24 +156,24 @@ export default {
   padding: 1.5rem;
   border: 1px solid $gray4;
 
-  &__logo{
-      width: 11rem;
+  &__logo {
+    width: 11rem;
   }
-  &__form{
-      border-top: 1px solid $gray4;
-      padding-top: 1.5rem;
+  &__form {
+    border-top: 1px solid $gray4;
+    padding-top: 1.5rem;
 
-    &__submit{
-        padding-top: 1.5rem;
-         .button{
-            width: 100%;
-        }
+    &__submit {
+      padding-top: 1.5rem;
+      .button {
+        width: 100%;
+      }
     }
-    a:not(.button){
-        color: $gray6;
+    a:not(.button) {
+      color: $gray6;
     }
-    small{
-        font-size: .79rem;
+    small {
+      font-size: 0.79rem;
     }
   }
 }
