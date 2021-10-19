b4fc87c8cfddd0fb1e9002dcedbe3135df9897dd
CodeMastah <paul@project-x.site>
2021-10-19 14:21:09 -0400

    Updated task-sidebar component

--- components/TaskSidebar/index.vue
+++ components/TaskSidebar/index.vue
@@ -1,232 +1,108 @@
 <template>
-  <div class="sidebar">
-    <div class="toolbox justify-end">
-      <a
-        data-v-4f2d8eb2=""
-        class="icon m-auto"
-        style="width: 1rem; height: 1rem"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="48"
-          height="48"
-          viewBox="0 0 48 48"
-        >
-          <g>
-            <g>
-              <path
-                d="M18.6,26.6,8,37.2V30.1A2.1,2.1,0,0,0,6.3,28,2,2,0,0,0,4,30V42a2,2,0,0,0,2,2H17.9A2.1,2.1,0,0,0,20,42.3,2,2,0,0,0,18,40H10.8L21.3,29.5a2.1,2.1,0,0,0,.3-2.7A1.9,1.9,0,0,0,18.6,26.6Z"
-              ></path>
-              <path
-                d="M30,4a2,2,0,0,0-2,2.3A2.1,2.1,0,0,0,30.1,8h7.1L26.7,18.5a2,2,0,0,0-.2,2.8A1.8,1.8,0,0,0,28,22a2,2,0,0,0,1.4-.6L40,10.8v7.1A2.1,2.1,0,0,0,41.7,20,2,2,0,0,0,44,18V6a2,2,0,0,0-2-2Z"
-              ></path>
-            </g>
-          </g>
-        </svg>
-      </a>
+  <bib-panel-wrapper class="side-panel">
+    <template v-slot:header>
+      <div class="side-panel__header">
+        <div class="side-panel__header__file__info">
+          <div
+            class="
+              p-05
+              of-hidden
+              text-of-elipsis
+              h-fit
+              text-wrap text-secondary
+            "
+          ></div>
+        </div>
+        <div class="d-flex side-panel__header__actions pull-right">
+          <div class="m-auto cursor-pointer">
+            <bib-icon
+              icon="full-screen"
+              variant="gray5"
+              :scale="1.25"
+            ></bib-icon>
+          </div>
+          <div class="m-auto cursor-pointer">
+            <bib-icon icon="download" variant="gray5" :scale="1.25"></bib-icon>
+          </div>
+          <div class="m-auto cursor-pointer">
+            <bib-icon icon="heart" variant="gray5" :scale="1.25"></bib-icon>
+          </div>
+          <div class="m-auto cursor-pointer">
+            <bib-icon
+              icon="comment-forum"
+              variant="gray5"
+              :scale="1.25"
+            ></bib-icon>
+          </div>
+          <div class="m-auto cursor-pointer">
+            <bib-icon icon="share" variant="gray5" :scale="1.25"></bib-icon>
+          </div>
+          <div class="m-auto cursor-pointer">
+            <bib-icon
+              icon="horizontal-dots"
+              variant="gray5"
+              :scale="1.25"
+            ></bib-icon>
+          </div>
+          <div class="m-auto cursor-pointer" @click="hideSidebar()">
+            <bib-icon icon="close" variant="gray4" :scale="1.5"></bib-icon>
+          </div>
+        </div>
+        <div class="row" style="margin: 1rem -0.5rem">
+          <div class="col-8">
+            <bib-input
+              type="text"
+              v-model="form.name"
+              placeholder="Enter task name..."
+              label="Task name"
+            ></bib-input>
+          </div>
 
-      <a
-        data-v-4f2d8eb2=""
-        class="icon m-auto"
-        style="width: 1rem; height: 1rem"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="48"
-          height="48"
-          viewBox="0 0 48 48"
-        >
-          <g>
-            <path
-              d="M43.4,29.4l-3.2-3.2A4.5,4.5,0,0,1,39,23.3V17C39,8.9,33.6,2,24,2S9,8.7,9,17v7a2.6,2.6,0,0,1-.7,1.7L4.6,29.4A2,2,0,0,0,4,30.8V38a2,2,0,0,0,2,2H17.1a7,7,0,0,0,13.8,0H42a2,2,0,0,0,2-2V30.8A2,2,0,0,0,43.4,29.4ZM40,36H8V31.7l3.1-3.2A6.4,6.4,0,0,0,13,24V17c0-5.3,2.9-11,11-11s11,5.9,11,11v6.3A8.6,8.6,0,0,0,37.3,29L40,31.7Z"
-            ></path>
-          </g>
-        </svg>
-      </a>
+          <div class="col-4">
+            <bib-input
+              type="date"
+              v-model="form.dueDate"
+              placeholder="Enter date/range"
+              label="Due date"
+            ></bib-input>
+          </div>
+        </div>
+      </div>
+    </template>
+    <template v-slot:menu>
+      <div class="menu">
+        <div class="w-50">
+          <bib-tabs
+            :value="activeFileInfoTab"
+            @change="fileInfoTabChange"
+            :tabs="fileInfoTabs"
+          ></bib-tabs>
+        </div>
+      </div>
+    </template>
+    <template v-slot:content>
+      <sidebar-overview
+        v-if="activeFileInfoTab == 'Overview'"
+        :fields="taskFields"
+        :tasks="tasks"
+      />
 
-      <a
-        data-v-4f2d8eb2=""
-        class="icon m-auto"
-        style="width: 1rem; height: 1rem"
-        href="#"
-      >
-        <svg
-          viewBox="11.681 37.209 472.743 429.612"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g
-            transform="matrix(10.744102, 0, 0, 10.744102, -9.805153, -5.767217)"
-            style=""
-          >
-            <path
-              d="M34,8.1a7.6,7.6,0,0,1,5.6,2.4A7.7,7.7,0,0,1,42,16.1c0,2.1-1.3,4.5-3.9,7.4L24,38.5,9.9,23.5C7.3,20.6,6,18.2,6,16.1a7.7,7.7,0,0,1,2.4-5.6A7.6,7.6,0,0,1,14,8.1c2.2,0,5.1,2,6.5,4.5L23.1,17a1.1,1.1,0,0,0,1.8,0l2.6-4.4c1.4-2.5,4.3-4.5,6.5-4.5M34,4c-3.9,0-8,3.1-10,6.5C22,7.1,17.9,4,14,4A12,12,0,0,0,5.5,7.6,11.9,11.9,0,0,0,2,16.1c0,4.1,2.7,7.6,5,10.2L23.3,43.7a1,1,0,0,0,1.4,0L41,26.3c2.3-2.6,5-6.1,5-10.2A12.1,12.1,0,0,0,34,4Z"
-            ></path>
-          </g>
-        </svg>
-      </a>
-
-      <a
-        data-v-4f2d8eb2=""
-        class="icon m-auto"
-        style="width: 1rem; height: 1rem"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="48"
-          height="48"
-          viewBox="0 0 48 48"
-        >
-          <g>
-            <path
-              d="M43.9,15H34V8a2,2,0,0,0-2.1-2H4.1A2,2,0,0,0,2,8V36a1,1,0,0,0,1,1,1.3,1.3,0,0,0,.8-.3L13,28h1v7a2,2,0,0,0,2.1,2H35l9.2,8.7a1.3,1.3,0,0,0,.8.3,1,1,0,0,0,1-1V17A2,2,0,0,0,43.9,15ZM11.4,24l-1.1,1.1L6,29.1V10H30V24H11.4ZM42,38.1l-4.3-4L36.6,33H18V28H31.9A2,2,0,0,0,34,26V19h8Z"
-            ></path>
-          </g>
-        </svg>
-      </a>
-
-      <a
-        data-v-4f2d8eb2=""
-        class="icon m-auto"
-        style="width: 1rem; height: 1rem"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 48 48"
-        >
-          <g>
-            <path
-              d="M24,46A12,12,0,0,1,12,34V25a7,7,0,0,1,14,0v9a2,2,0,0,1-4,0V25a3,3,0,0,0-3-3,3.1,3.1,0,0,0-3,3v9a8,8,0,0,0,16,0V14a8,8,0,0,0-16,0,2,2,0,0,1-4,0,12,12,0,0,1,24,0V34A12,12,0,0,1,24,46Z"
-            ></path>
-          </g>
-        </svg>
-      </a>
-
-      <a
-        data-v-4f2d8eb2=""
-        class="icon m-auto"
-        style="width: 1rem; height: 1rem"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="48"
-          height="48"
-          viewBox="0 0 48 48"
-        >
-          <g>
-            <path
-              d="M31.2,14.2,41,24.1l-9.8,9.8V26.8L27,27c-6.8.3-12,1-16.1,2.4,3.6-3.8,9.3-6.8,16.7-7.5l3.6-.3V14.2M28.3,6a1.2,1.2,0,0,0-1.1,1.3V17.9C12,19.4,2.2,29.8,2,40.3c0,.6.2,1,.6,1s.7-.3,1.1-1.1c2.4-5.4,7.8-8.5,23.5-9.2v9.7A1.2,1.2,0,0,0,28.3,42a.9.9,0,0,0,.8-.4L45.6,25.1a1.5,1.5,0,0,0,0-2L29.1,6.4a.9.9,0,0,0-.8-.4Z"
-            ></path>
-          </g>
-        </svg>
-      </a>
-
-      <a
-        data-v-4f2d8eb2=""
-        class="icon m-auto"
-        style="width: 1rem; height: 1rem"
-        href="#"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="48"
-          height="48"
-          viewBox="0 0 48 48"
-        >
-          <title>trash</title>
-          <g id="Layer_2" data-name="Layer 2">
-            <g id="invisible_box" data-name="invisible box">
-              <rect width="48" height="48" fill="none"></rect>
-            </g>
-            <g id="icons_Q2" data-name="icons Q2">
-              <path
-                d="M43,8.8a2.3,2.3,0,0,1-.6,1.6A1.7,1.7,0,0,1,41,11H29a2,2,0,0,1-2-2V7H21V9a2,2,0,0,1-2,2H7.1A2.1,2.1,0,0,1,5,9.2a2.3,2.3,0,0,1,.6-1.6A1.7,1.7,0,0,1,7,7H17V5a2,2,0,0,1,2-2H29a2,2,0,0,1,2,2V7h9.9A2.1,2.1,0,0,1,43,8.8Z"
-              ></path>
-              <path
-                d="M34.8,16.8,32.4,41H15.6L13.2,16.8a2,2,0,0,0-2-1.8h0a2,2,0,0,0-2,2.2l2.6,26a2,2,0,0,0,2,1.8H34.2a2,2,0,0,0,2-1.8l2.6-26a2,2,0,0,0-2-2.2h0A2,2,0,0,0,34.8,16.8Z"
-              ></path>
-            </g>
-          </g>
-        </svg>
-      </a>
-
-      <a
-        data-v-4f2d8eb2=""
-        class="icon m-auto"
-        style="width: 1rem; height: 1rem"
-        href="#"
-        @click="hideSidebar()"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="48"
-          height="48"
-          viewBox="0 0 48 48"
-        >
-          <title>close</title>
-          <g id="Layer_2" data-name="Layer 2">
-            <g id="invisible_box" data-name="invisible box">
-              <rect width="48" height="48" fill="none"></rect>
-            </g>
-            <g id="icons_Q2" data-name="icons Q2">
-              <path
-                d="M26.8,24,37.4,13.5a2.1,2.1,0,0,0,.2-2.7,1.9,1.9,0,0,0-3-.2L24,21.2,13.4,10.6a1.9,1.9,0,0,0-3,.2,2.1,2.1,0,0,0,.2,2.7L21.2,24,10.6,34.5a2.1,2.1,0,0,0-.2,2.7,1.9,1.9,0,0,0,3,.2L24,26.8,34.6,37.4a1.9,1.9,0,0,0,3-.2,2.1,2.1,0,0,0-.2-2.7Z"
-              ></path>
-            </g>
-          </g>
-        </svg>
-      </a>
-    </div>
-
-    <div class="row">
-      <div class="col-8">
-        <bib-input
-          type="text"
-          v-model="form.name"
-          placeholder="Enter task name..."
-          label="Task name"
-        ></bib-input>
+      <div class="container pt-1">
+        <task-group v-if="activeFileInfoTab == 'Subtasks'" />
       </div>
 
-      <div class="col-4">
-        <bib-input
-          type="date"
-          v-model="form.dueDate"
-          placeholder="Enter date/range"
-          label="Due date"
-        ></bib-input>
-      </div>
-    </div>
+      <sidebar-team v-if="activeFileInfoTab == 'Team'" />
 
-    <bib-tabs :value="activeTab" @change="tabChange" :tabs="tabs"> </bib-tabs>
+      <sidebar-conversation v-if="activeFileInfoTab == 'Conversations'" />
 
-    <sidebar-overview
-      v-if="activeTab == tabTiles.overview"
-      :fields="taskFields"
-      :tasks="tasks"
-    />
+      <sidebar-files v-if="activeFileInfoTab == 'Files'" />
 
-    <div class="container pt-1">
-      <task-group v-if="activeTab === tabTiles.subtasks" />
-    </div>
-
-    <sidebar-team v-if="activeTab === tabTiles.team" />
-
-    <sidebar-conversation v-if="activeTab === tabTiles.conversations" />
-
-    <sidebar-files v-if="activeTab === tabTiles.files" />
-
-    <sidebar-history v-if="activeTab === tabTiles.history" />
-  </div>
+      <sidebar-history v-if="activeFileInfoTab == 'History'" />
+    </template>
+  </bib-panel-wrapper>
 </template>
-
-<script>
+  
+  <script>
 import { mapGetters } from "vuex";
 
 export default {
@@ -236,8 +112,7 @@ export default {
         name: "",
         dueDate: "2021/11/7",
       },
-      activeTab: "Overview",
-      tabs: [
+      fileInfoTabs: [
         "Overview",
         "Subtasks",
         "Team",
@@ -271,14 +146,7 @@ export default {
           label: "Due Date",
         },
       ],
-      tabTiles: {
-        overview: "Overview",
-        subtasks: "Subtasks",
-        team: "Team",
-        conversations: "Conversations",
-        files: "Files",
-        history: "History",
-      },
+      activeFileInfoTab: "Overview",
     };
   },
   computed: {
@@ -287,32 +155,17 @@ export default {
     }),
   },
   methods: {
-    tabChange(value) {
-      this.activeTab = value;
-    },
     hideSidebar() {
-      document.querySelector("#__layout").classList.add("hide-sidebar");
+      document.querySelector("#__layout").classList.remove("show-sidebar");
+    },
+    fileInfoTabChange(value) {
+      this.activeFileInfoTab = value;
     },
   },
 };
 </script>
-
+  
 <style lang="scss" scoped>
-.sidebar {
-  position: fixed;
-  top: 4rem;
-  right: 0;
-  height: calc(100% - 4rem);
-  overflow-y: scroll;
-  overflow-x: hidden;
-  width: 36%;
-  min-width: 280px;
-  margin-left: auto;
-  box-shadow: 1px 3px 2px 3px #ddd;
-  background: #fff;
-  z-index: 10;
-}
-
 svg {
   width: 100%;
   height: 100%;
@@ -339,4 +192,10 @@ svg {
 .container.pt-1::v-deep {
   padding-top: 1rem;
 }
+
+.d-flex {
+  width: 50%;
+  margin-left: auto;
+}
 </style>
+  
