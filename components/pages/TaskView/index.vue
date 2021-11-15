137e0e318b0fd08d8d4223303d04b4bc90dc45e1
CodeMastah <paul@project-x.site>
2021-11-15 07:01:38 -0500

    Integrated SSO API & Updated table

--- components/pages/TaskView/index.vue
+++ components/pages/TaskView/index.vue
@@ -3,43 +3,151 @@
     <task-actions :gridType="gridType"></task-actions>
 
     <template v-if="gridType === 'list'">
-      <custom-table
+      <bib-table
         :fields="tableFields"
-        class="bg-white no-content"
-        :contentless="true"
+        :sections="tableSections.slice(0, 3)"
+        :collapseObj="{
+          collapsed: false,
+          label: 'Section',
+        }"
+        class="border-gray4 bg-white"
+        :style="{ borderBottom: 'none' }"
+        @item-clicked="toggleSidebar"
       >
-      </custom-table>
+        <template #cell(name)="data">
+          <div class="d-flex gap-05">
+            <bib-avatar class="mt-auto mb-auto" size="1.5rem"></bib-avatar>
+            <span class="text-dark">{{ data.value.name }}</span>
+          </div>
+        </template>
+        <template #cell(status)="data">
+          <div class="justify-between text-dark">
+            <span :class="statusClass(data.value.status)">
+              {{ data.value.status }}
+            </span>
+            <span :class="statusClass(data.value.status)">
+              {{ data.value.progress }}</span
+            >
+          </div>
+        </template>
+        <template #cell(priority)="data">
+          <div class="justify-between text-dark">
+            <span :class="priorityClass(data.value.priority)">
+              {{ data.value.priority }}
+            </span>
+          </div>
+        </template>
+        <template #cell(assignee)="data">
+          <div class="text-dark">
+            <bib-avatar class="mt-auto mb-auto" size="1.5rem"></bib-avatar>
 
-      <section>
-        <task-list-section
-          :headless="true"
-          label="Section"
-          :taskFields="tableFields"
-          :taskSections="tableSections.slice(0, 3)"
-          :open="true"
-          groupName="1" 
-        />
-      </section>
+            <span>{{ data.value.assignee }}</span>
+          </div>
+        </template>
+        <template #cell(dueDate)="data">
+          <div class="text-dark">
+            <span>{{ data.value.dueDate }}</span>
+          </div>
+        </template>
+      </bib-table>
 
-      <section>
-        <task-list-section
-          :headless="true"
-          label="Section"
-          :taskFields="tableFields"
-          :taskSections="tableSections.slice(3, 6)"
-          groupName="1"
-        />
-      </section>
+      <bib-table
+        :fields="tableFields"
+        :sections="tableSections.slice(3, 6)"
+        :headless="true"
+        :collapseObj="{
+          collapsed: true,
+          label: 'Section',
+        }"
+        class="border-gray4 bg-white"
+        :style="{ borderBottom: 'none' }"
+        @item-clicked="toggleSidebar"
+      >
+        <template #cell(name)="data">
+          <div class="d-flex gap-05">
+            <bib-avatar class="mt-auto mb-auto" size="1.5rem"></bib-avatar>
+            <span class="text-dark">{{ data.value.name }}</span>
+          </div>
+        </template>
+        <template #cell(status)="data">
+          <div class="justify-between text-dark">
+            <span :class="statusClass(data.value.status)">
+              {{ data.value.status }}
+            </span>
+            <span :class="statusClass(data.value.status)">
+              {{ data.value.progress }}</span
+            >
+          </div>
+        </template>
+        <template #cell(priority)="data">
+          <div class="justify-between text-dark">
+            <span :class="priorityClass(data.value.priority)">
+              {{ data.value.priority }}
+            </span>
+          </div>
+        </template>
+        <template #cell(assignee)="data">
+          <div class="text-dark">
+            <bib-avatar class="mt-auto mb-auto" size="1.5rem"></bib-avatar>
 
-      <section>
-        <task-list-section
-          :headless="true"
-          label="Section"
-          :taskFields="tableFields"
-          :taskSections="tableSections.slice(6, 9)"
-          groupName="1"
-        />
-      </section>
+            <span>{{ data.value.assignee }}</span>
+          </div>
+        </template>
+        <template #cell(dueDate)="data">
+          <div class="text-dark">
+            <span>{{ data.value.dueDate }}</span>
+          </div>
+        </template>
+      </bib-table>
+
+      <bib-table
+        :fields="tableFields"
+        :sections="tableSections.slice(6, 9)"
+        :headless="true"
+        :collapseObj="{
+          collapsed: true,
+          label: 'Section',
+        }"
+        class="border-gray4 bg-white"
+        :style="{ borderBottom: 'none' }"
+        @item-clicked="toggleSidebar"
+      >
+        <template #cell(name)="data">
+          <div class="d-flex gap-05">
+            <bib-avatar class="mt-auto mb-auto" size="1.5rem"></bib-avatar>
+            <span class="text-dark">{{ data.value.name }}</span>
+          </div>
+        </template>
+        <template #cell(status)="data">
+          <div class="justify-between text-dark">
+            <span :class="statusClass(data.value.status)">
+              {{ data.value.status }}
+            </span>
+            <span :class="statusClass(data.value.status)">
+              {{ data.value.progress }}</span
+            >
+          </div>
+        </template>
+        <template #cell(priority)="data">
+          <div class="justify-between text-dark">
+            <span :class="priorityClass(data.value.priority)">
+              {{ data.value.priority }}
+            </span>
+          </div>
+        </template>
+        <template #cell(assignee)="data">
+          <div class="text-dark">
+            <bib-avatar class="mt-auto mb-auto" size="1.5rem"></bib-avatar>
+
+            <span>{{ data.value.assignee }}</span>
+          </div>
+        </template>
+        <template #cell(dueDate)="data">
+          <div class="text-dark">
+            <span>{{ data.value.dueDate }}</span>
+          </div>
+        </template>
+      </bib-table>
     </template>
 
     <template v-else>
@@ -83,7 +191,25 @@ export default {
     return {
       tableFields: DUMMY_TASK_FIELDS,
       tableSections: DUMMY_TASKS,
+      flag: false,
     };
+  },
+  methods: {
+    toggleSidebar() {
+      this.flag = !this.flag;
+      this.$root.$emit("open-sidebar", this.flag);
+    },
+
+    statusClass(status) {
+      if (status === "Past Due") return "text-red";
+      if (status === "In-progress") return "text-blue";
+    },
+
+    priorityClass(priority) {
+      if (priority === "Urgent") return "text-red";
+      if (priority === "Top") return "text-orange";
+      return "text-green";
+    },
   },
 };
 </script>
