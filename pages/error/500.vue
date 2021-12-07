f85025d6d5646e7a38502aee489fb2bb2f48d701
CodeMastah <paul@project-x.site>
2021-12-07 17:35:36 -0500

    fix: 500 error test.
    - statusCode error fix.

--- pages/error/500.vue
+++ pages/error/500.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 v-if="error.statusCode === 500">Something is going wrong</h1>
+    <h1 v-if="error && error.statusCode === 500">Something is going wrong</h1>
     <h1 v-else>An error occurred</h1>
     <NuxtLink to="/">Redirect to Home</NuxtLink>
   </div>
