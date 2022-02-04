107d367919ccea5c314c4cb8ff091005f35af1bd
CodeMastah <paul@project-x.site>
2022-02-04 18:10:32 +0530

    verifyToken API on frontend

--- store/token.js
+++ store/token.js
@@ -1,7 +1,7 @@
 
 export const state = () => ({
-    token: null,
-    // token: ''
+    // token: null,
+    token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrNjFZUWRKNko3bGRPR3BKIiwic3ViZSI6ImRocnV2LnNoYXJtYUBxc3N0ZWNobm9zb2Z0LmNvbSIsInN1YnMiOiJBQ1RJVkUiLCJzdWJiIjoiTzNHV3BtYms1ZXpKbjRLUiIsInN1YmJzIjoiQ0xJRU5UIiwic3ViciI6IkFETUlOIiwic3ViYyI6IkNhbmFkYSIsImlhdCI6MTY0Mzg5MTkyODA1NiwiZXhwIjoxNjUxNjY3OTI4MDU2LCJqdGkiOiI5ZjUxOWZiYy00NTFiLTQ0ZmMtYjM0MS1lYWVjYjY0MGM5MWIifQ.taqZq0UfB8_0p9g7pEA969C1v1kdpI0de9rXQch96aE'
   });
   
   export const mutations = {
