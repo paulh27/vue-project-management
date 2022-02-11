c26fe560e07ba2b6ca00701083e4f8f14f8373fe
CodeMastah <paul@project-x.site>
2022-02-11 16:31:36 +0530

    refactor default template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,54 +1,173 @@
 <template>
   <div>
-    <Nuxt />
+    <bib-app-wrapper class="test" :navigationCollapsed="collapseNavigation" :select="appHeaderActions.select" :button="appHeaderActions.button" @button-click="rightClkFileSection" @collapseNavigation="
+        () => {
+          resizeCalendar()
+          collapseNavigation = !collapseNavigation;
+        }
+      ">
+      <template #topbar>
+        <bib-header></bib-header>
+      </template>
+      <template #switcher>
+        <bib-app-switcher :menuItems="appItems"></bib-app-switcher>
+      </template>
+      <template #navigation>
+        <bib-app-navigation :items="navItems1" @click='goToRoute($event)' class="mb-1"></bib-app-navigation>
+        <bib-app-navigation :items="navItems2" @click='goToRoute($event)'></bib-app-navigation>
+        <!-- <bib-app-navigation :items="navItems3"></bib-app-navigation> -->
+      </template>
+      <template #content>
+        <div class="main" id='main-content' :class="openSidebar ? 'open-sidebar' : ''">
+          <Nuxt />
+        </div>
+      </template>
+    </bib-app-wrapper>
   </div>
 </template>
-
 <script>
+import { mapState, mapGetters } from "vuex";
 
-  export default {
-    async created() {
-      if(process.client) {
+export default {
+  data() {
+    return {
+      openSidebar: false,
+      appItems: [{
+          img: "Layers",
+          color: "primary",
+          text: "Templates",
+          href: process.env.VITE_BIB_TEMPLATES_APP_URL,
+        },
+        { img: "Files", color: "success-sub1", text: "Files", href: process.env.VITE_BIB_DRIVE_URL },
+        { img: "Signature", color: "orange", text: "eSign", href: process.env.VITE_BIB_ESIGN_APP_URL },
+        { img: "Tick", color: "primary", text: "Project", active: true, href: process.env.VUE_APP_URL },
+        { img: "CommentForum", color: "purple", text: "Chat", href: process.env.VITE_BIB_CHAT_APP_URL },
+      ],
+      navItems1: [
+        { label: "Home", icon: "home", key:"dashboard-route" },
+        { label: "Inbox", icon: "mail-new" },
+        { label: "My tasks", icon: "check-circle" },
+        { label: "Favorites", icon: "heart-like" },
+      ],
+      navItems2: [
+        { label: "Tasks", icon: "check-circle", key:"task-route" },
+        { label: "Projects", icon: "folder-add", key: 'project-route', selected: true },
+        { label: "Goals", icon: "plans" },
+        { label: "Dream", icon: "star" },
+      ],
+      navItems3: [
+        { label: "Departments", icon: "home" },
+        { label: "People", icon: "user-group" },
+      ],
+      collapseNavigation: false,
+      appHeaderActions: {
+        button: {
+          label: "Create / Upload",
+          event: "button-click",
+          variant: "success",
+          icon: "add",
+        },
+        select: {
+          items: [{
+              label: "Organization Name",
+              event: "item-click",
+              img: "https://i.pravatar.cc/300",
+            },
+            {
+              label: "Organization Name",
+              event: "item-click",
+              img: "https://i.pravatar.cc/300",
+            },
+          ],
+          footer: {
+            label: "Add organization",
+            icon: "plus",
+            event: "add-organization",
+          },
+        },
+      },
+    }
+  },
+  created() {
+    this.$root.$on("open-sidebar", (flag) => {
+      this.openSidebar = flag;
+    });
+  },
+  mounted() {
+    if (process.client) {
 
-        const jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0cTJWb2xlalJlak5tR1FCIiwic3ViZSI6Imh0YW5nQGJpenRyZWUuY29tIiwic3VicyI6IkFDVElWRSIsInN1YmIiOiJPM0dXcG1iazVlekpuNEtSIiwic3ViYnMiOiJDTElFTlQiLCJzdWJyIjoiQURNSU4iLCJzdWJjIjoiQ2FuYWRhIiwiaWF0IjoxNjQzOTc2NDcxMDE2LCJleHAiOjE2NTE3NTI0NzEwMTYsImp0aSI6IjU0YjM4ZjQ1LWI0YTgtNDQ4Yy1iMDZhLWQ0ZTk5Y2NhY2UzNCJ9.2WuLnfCQVVOBGB6BACi4QJC8zJIm_7UEDljFZIT2950';
-        this.$cookies.set('b_ssojwt', jwt);
+      const jwt = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0cTJWb2xlalJlak5tR1FCIiwic3ViZSI6Imh0YW5nQGJpenRyZWUuY29tIiwic3VicyI6IkFDVElWRSIsInN1YmIiOiJPM0dXcG1iazVlekpuNEtSIiwic3ViYnMiOiJDTElFTlQiLCJzdWJyIjoiQURNSU4iLCJzdWJjIjoiQ2FuYWRhIiwiaWF0IjoxNjQzOTc2NDcxMDE2LCJleHAiOjE2NTE3NTI0NzEwMTYsImp0aSI6IjU0YjM4ZjQ1LWI0YTgtNDQ4Yy1iMDZhLWQ0ZTk5Y2NhY2UzNCJ9.2WuLnfCQVVOBGB6BACi4QJC8zJIm_7UEDljFZIT2950';
+      this.$cookies.set('b_ssojwt', jwt);
 
-        if (this.$cookies.get('b_ssojwt')) {
-              let jwt = this.$cookies.get('b_ssojwt');
+      if (this.$cookies.get('b_ssojwt')) {
+        let jwt = this.$cookies.get('b_ssojwt');
 
-              // extract user
-              var base64Url = jwt.split('.')[1];
-              var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-              var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-                  return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-              }).join(''));
+        // extract user
+        var base64Url = jwt.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
 
-              let user = JSON.parse(jsonPayload);
+        let user = JSON.parse(jsonPayload);
 
-              await this.$axios
-              .$post(process.env.VUE_APP_API_ENDPOINT + "/user/create", 
-              { 
-                id: user.sub, 
-                email:  user.sube
-              }).then((value) => {
-                console.log('user created!!')
-              }).catch((err) => {
-                console.log('there was some issue!!!')
-              })
+        this.$axios
+          .$post(process.env.VUE_APP_API_ENDPOINT + "/user/create", {
+            id: user.sub,
+            email: user.sube
+          }).then((value) => {
+            console.log('user created!!')
+          }).catch((err) => {
+            console.log('there was some issue!!!')
+          })
 
-              this.$store.dispatch('token/setToken', jwt);
-              localStorage.setItem('accessToken', jwt)
-        } else {
-              window.location.href = process.env.AUTH_REDIRECT_URL + process.env.VUE_APP_URL;
-        }
+        this.$store.dispatch('token/setToken', jwt);
+        localStorage.setItem('accessToken', jwt)
+      } else {
+        window.location.href = process.env.AUTH_REDIRECT_URL + process.env.VUE_APP_URL;
+      }
+    }
+  },
+  methods: {
+
+    rightClkFileSection(event) {
+      this.$refs.modals.showCreateProjectModal = true;
+    },
+    
+    resizeCalendar() {
+      if (document.getElementById('myDiv')) {
+        window.dispatchEvent(new Event('resize'));
+      }
+      return false;
+    },
+    
+
+    goToRoute($event) {
+      console.log($event.key)
+      if ($event.key == 'dashboard-route') {
+        this.$router.push('/dashboard')
+      }
+      if ($event.key == 'project-route') {
+        this.$router.push('/projects')
+      }
+      if ($event.key == 'task-route') {
+        this.$router.push('/tasks')
+      }
+    },
+
+    // Handle User logout
+    async logout() {
+      try {
+        let response = await this.$auth.logout();
+      } catch (err) {
+        console.log(err);
       }
     },
   }
-  
+}
+
 </script>
-
-
-<style>
+<style lang="scss">
 html {
   font-family: 'Lato', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
@@ -95,4 +214,48 @@ html {
   color: #fff;
   background-color: #35495e;
 }
+.main {
+  /*display: grid;
+  grid-template-rows: 2.5rem 2.5rem 2.5rem 100%;*/
+  overflow: hidden;
+  background-color: white;
+
+  .menu {
+    padding: 0 1rem 0 0.25rem;
+  }
+
+  .bread {
+    padding: 0 0.15rem 0 0.15rem;
+    display: grid;
+    grid-template-columns: 2rem auto;
+    line-height: 1.8rem;
+  }
+}
+::v-deep {
+  .panel-wrapper.side-panel {
+    position: fixed;
+    right: 0;
+    max-width: $sidebar-width;
+    border-left: 1px solid $gray4;
+  }
+
+  .align-start.d-flex.flex-d-column {
+    padding-left: 0 !important;
+  }
+
+  .app-wrapper__collapser {
+    justify-content: center;
+  }
+
+  .app-wrapper__collapser--collapsed {
+    right: 0 !important;
+  }
+
+  .app-wrapper__navigation--collapsed {
+    .nav-item {
+      padding: 0;
+    }
+  }
+}
+
 </style>
