5f463603064724ce3d4c6986b98d5f95e5219ec1
CodeMastah <paul@project-x.site>
2022-02-22 15:24:25 +0530

    user Info plugin

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="text-center">
-    <h2 class="p-2 text-secondary">Welcome </h2>
-    
+    <h2 class="p-2 text-secondary">Welcome {{name}}</h2>
+    <!-- <p>{{$userInfo('k61YQdJ6J7ldOGpJ')}}</p> -->
   </div>
 </template>
 
 <script>
+import {mapGetters} from 'vuex';
+
   export default {
-    name: 'dashboard'
+    name: 'dashboard',
+    data() {
+      return {
+        name: ''
+      }
+    },
+    computed: {
+      ...mapGetters({
+        user: 'user/getUser'
+      })
+    },
+    mounted() {
+      this.$userInfo(this.user.sub).then((res) => this.name = res)
+    }
   }
 </script>
