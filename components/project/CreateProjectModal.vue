1b0f385709c2bff26b3b22dcc4dcccf281d8201a
CodeMastah <paul@project-x.site>
2022-04-29 13:06:55 +0530

    create project assignee all members

--- components/project/CreateProjectModal.vue
+++ components/project/CreateProjectModal.vue
@@ -6,19 +6,19 @@
         <bib-input v-model.trim="projectName" placeholder="Name your project"></bib-input>
         <small class="text-danger mb-05" style="margin-top:-0.5rem; display:block;">{{projectName ? '' : 'Project name is required'}}</small>
         <bib-input label="Department" v-model="department" placeholder="Type or select department name"></bib-input>
-        <label id="create-project-modal-heading" class="text-gray6">Assign a project lead</label>
+        <label id="create-project-modal-heading" class="text-gray6">Assign a project lead <span class="text-danger">*</span></label>
         <bib-button test_id="create-project-dd1" dropdown1="add" label="Type name or email" v-model="owner" v-on:input-keydown="dropdownInputKeydown" :footer="{icon: 'add', label: 'Invite via email', event: 'footer-action'}" @footer-action="inviteViaEmail" class="mt-05 mb-05">
           <template v-slot:menu>
             <ul id="cpm-fields" class="border-gray1" style="border-radius: 0 !important; border: 1px solid var(--bib-gray1);">
               <li :id="'cpm-field-'+index" v-for="(tm, index) in filterUser" :key="'cpm-item-'+index" v-on:click="dd1ItemClick(tm)">
-                <bib-avatar size="1.5rem"></bib-avatar>
+                <bib-avatar :src="tm.avatar" size="1.25rem"></bib-avatar>
                 <span :id="'cpm-person-name'+index" class="ml-05"> {{tm.label}} <span class="ml-075">{{tm.email}}</span></span>
               </li>
             </ul>
           </template>
         </bib-button>
-        <div id="project-team-members" class="d-flex pt-025" >
-          <email-chip v-if="Object.keys(owner).length > 0" :name="`${owner.firstName} ${owner.lastName}`" :email="owner.email ? owner.email : owner.sube" :text="(owner.email ? owner.email[0] : '') || (owner.sube ? owner.sube[0] : '') "  v-bind:close="true" v-on:remove-email="removeOwner"></email-chip>
+        <div id="project-team-members" class="d-flex pt-025">
+          <email-chip v-if="Object.keys(owner).length > 0" :name="owner.label" :email="owner.email ? owner.email : owner.sube" :avatar="owner.avatar" v-bind:close="true" v-on:remove-email="removeOwner"></email-chip>
           <small v-else class="text-danger">Project owner is required</small>
         </div>
         <!-- <bib-input label="Enter email" placeholder="Enter email"></bib-input> -->
@@ -31,13 +31,14 @@
         <loading :loading="loading"></loading>
       </template>
       <template v-slot:footer>
+        <!-- <div v-if="error" class="shape-rounded border-danger px-075 py-05 text-danger font-sm">{{errorMsg}}</div> -->
         <div class="m-auto pt-1 d-flex justify-between" id='tm-create-project-model'>
           <bib-button @click.native="
               () => {
                 showCreateProjectModal = false;
               }
             " variant="light" size="lg" pill label="Cancel"></bib-button>
-          <bib-button @click.native="createProject()" variant="success" size="lg" id="create-project-btn" pill label="Create" ></bib-button>
+          <bib-button @click.native="createProject()" variant="success" size="lg" id="create-project-btn" pill label="Create"></bib-button>
         </div>
       </template>
     </bib-modal-wrapper>
@@ -57,6 +58,7 @@ export default {
       projectlead: "Enter name or email",
       filterKey: "",
       error: false,
+      errorMsg: "",
       loading: false,
     };
   },
@@ -64,16 +66,16 @@ export default {
   computed: {
     ...mapGetters({
       user: "user/getUser",
-      // teammate: 'user/getTeamMembers'
-      companyUsers: "company/getCompanyMembers"
+      teamMembers: "user/getTeamMembers",
+      // companyUsers: "company/getCompanyMembers"
     }),
     filterUser() {
-      return this.companyUsers.filter((u) => {
+      return this.teamMembers.filter((u) => {
         if (u.email.indexOf(this.filterKey) >= 0) {
           return u
         }
       })
-    }
+    },
   },
   mounted() {
     // console.log(this.user)
@@ -100,7 +102,7 @@ export default {
     /*leadChange() {
       console.log(this.owner)
     },*/
-    removeOwner(){
+    removeOwner() {
       this.owner = {}
     },
     bindEnter(event, button) {
@@ -112,16 +114,26 @@ export default {
     createProject() {
       this.loading = true
       // console.log(this.projectName, this.owner)
-      let ownerId = this.owner.sub || this.owner.id
+      // let ownerId = this.owner.sub || this.owner.id
       // console.log(ownerId)
-      if (this.projectName && ownerId) {
-        this.$store.dispatch('project/createProject', { userId: ownerId, title: this.projectName }).then(()=>{
+      if (this.projectName && this.owner.id) {
+        this.$store.dispatch('project/createProject', { user: this.owner, title: this.projectName }).then((res) => {
+          // console.log(res)
           this.loading = false
-          this.showCreateProjectModal = false
+          if (res.statusCode == 200) {
+            this.showCreateProjectModal = false
+          }
+          alert(res.message)
+        }).catch(e => {
+          alert(e.message)
+          this.loading = false
         })
       } else {
         this.loading = false
-        this.error = true
+        console.error("required fields");
+        /*this.errorMsg = "Please fill required fields"
+        this.error = true*/
+
       }
       // this.$emit("create-project")
     },
