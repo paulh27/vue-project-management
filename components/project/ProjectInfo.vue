68b630d7d2c0a305306af17ccec0a3de4c5c4e98
CodeMastah <paul@project-x.site>
2022-06-21 16:14:07 +0530

    make the project details fetching efficient for myTask page

--- components/project/ProjectInfo.vue
+++ components/project/ProjectInfo.vue
@@ -1,7 +1,10 @@
 <template>
   <span :id="'project-info-'+ projectId" class="project-title text-dark text-truncate">{{projectTitle}}</span>
 </template>
+
 <script>
+import {mapGetters} from 'vuex';
+
 export default {
 
   name: 'ProjectInfo',
@@ -14,18 +17,34 @@ export default {
       projectTitle: null,
     }
   },
+
+  computed: {
+    ...mapGetters({
+      projects: 'project/getAllProjects'
+    })
+  },
+
   created() {
     if (this.projectId) {
-      this.$axios.get(`project/${this.projectId}`, {
-        headers: { 'Authorization': `Bearer ${localStorage.getItem('accessToken')}` }
-      }).then((res) => {
-        // console.log(res.data.data.title)
-        this.projectTitle = res.data.data.title;
-      }).catch(e => {
-        console.log(e);
+      // this.$axios.get(`project/${this.projectId}`, {
+      //   headers: { 'Authorization': `Bearer ${localStorage.getItem('accessToken')}` }
+      // }).then((res) => {
+      //   // console.log(res.data.data.title)
+      //   this.projectTitle = res.data.data.title;
+      // }).catch(e => {
+      //   console.log(e);
+      // })
+      this.projects.filter((el) => {
+        if(el.id == this.projectId) {
+          this.projectTitle = el.title;
+        }
       })
     }
-  }
+  },
+
+  // mounted() {
+  //   console.log(this.projects)
+  // }
 }
 
 </script>
