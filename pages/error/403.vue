8f71034b1fc718b1140a6c353d62bebb9abfefc7
CodeMastah <paul@project-x.site>
2022-09-29 17:57:25 +0530

    refresh page, same page different user selection, error handling

---
+++ pages/error/403.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="403-error-wrapper">
+    <h1 id="403-error-heading1">Not Enough permission to see this page</h1>
+    <h1 id="403-error-heading2">An error occurred</h1>
+    <NuxtLink to="/">Redirect to Home</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ["error"],
+  // layout: "error"
+};
+</script>
