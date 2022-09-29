5b1e6f10cec0e9fa4e037c261203a7ecebde9c6d
CodeMastah <paul@project-x.site>
2022-09-29 17:59:59 +0530

    project route validation

--- components/elements/Loading.vue
+++ components/elements/Loading.vue
@@ -1,6 +1,6 @@
 <template >
     <div v-if="loading" id="loading-comp" class="position-absolute d-flex align-center justify-center comp-loading">
-        <bib-spinner variant="primary"></bib-spinner>
+        <bib-spinner variant="primary"></bib-spinner> {{text}}
     </div>
 </template>
 <script>
@@ -15,7 +15,8 @@ export default {
         }
     },
     props: {
-    	loading: { type: Boolean, default: true }
+    	loading: { type: Boolean, default: true },
+        text: String,
     }
 }
 </script>
@@ -25,7 +26,7 @@ export default {
   right: 0;
   bottom: 0;
   left: 0;
-  background-color: rgba(250,250,250, 0.75);
+  background-color: rgba(250,250,250, 0.85);
   z-index: 29;
 }
 
--- pages/notfound/index.vue
+++ pages/notfound/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="d-flex align-center justify-center h-100">
-    <div class="card">
-      <h3>The project you're looking for does not exist or is deleted.</h3>
-      <p>Go to <nuxt-link to="/projects">Projects page</nuxt-link>
+    <div class="notfound">
+      <h2 class="text-danger">Not Found!</h2>
+      <h4>The project you're looking for, does not exist or is deleted.</h4>
+      <p>Go to <nuxt-link to="/projects" class="">Projects page</nuxt-link>
       </p>
     </div>
   </div>
@@ -20,5 +21,8 @@ export default {
 }
 
 </script>
-<style lang="css" scoped>
+<style lang="scss" scoped>
+  .notfound {
+    padding: 2.5rem; border-radius: 1rem; background-color: var(--bib-light)
+  }
 </style>
