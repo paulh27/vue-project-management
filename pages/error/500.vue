6a3e9b39ce654c422c48fca4a5cf46d4afa78ee8
CodeMastah <paul@project-x.site>
2022-11-25 17:34:14 +0530

    updated error pages UI

--- pages/error/500.vue
+++ pages/error/500.vue
@@ -1,14 +1,20 @@
 <template>
-  <div id="500-error-wrapper">
-    <h1 id="500-error-heading1" v-if="error && error.statusCode === 500">Something is going wrong</h1>
-    <h1 id="500-error-heading2" v-else>An error occurred</h1>
-    <NuxtLink to="/">Redirect to Home</NuxtLink>
-  </div>
+    <div id="error-500-wrapper" class="shape-rounded bg-light">
+      <h1 id="error-500-heading1" v-if="error && error.statusCode === 500">Something is going wrong</h1>
+      <h1 id="error-500-heading2" v-else>An error occurred</h1>
+      <NuxtLink class="error-link" to="/">Redirect to Home</NuxtLink>
+    </div>
 </template>
 
-<script>
-export default {
-  props: ["error"],
-  layout: "error"
-};
-</script>
+
+<style lang="scss" scoped>
+#error-500-wrapper {
+  max-width: 30rem;
+  margin: 50px auto;
+  text-align: center;
+  padding: 1rem;
+}
+.error-link {
+  text-decoration: underline;
+}
+</style>
