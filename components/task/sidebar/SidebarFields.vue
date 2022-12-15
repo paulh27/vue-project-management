c24355fd9475d59fede70773bdeaf41aab7c9b2d
CodeMastah <paul@project-x.site>
2022-12-15 11:01:24 +0530

    new component

---
+++ components/task/sidebar/SidebarFields.vue
@@ -0,0 +1,208 @@
+<template>
+  <div class="task-info position-relative px-1" id="task-input-wrap">
+    <div class="row mx-0" id="sidebar-row-1">
+      <div class="col-4" id="sidebar-col-1">
+        <bib-select label="Assignee" test_id="task_assignee_select" :options="orgUsers" v-model="form.userId" v-on:change="debounceUpdateField('Assignee', 'userId', form.userId)"></bib-select>
+      </div>
+      <div class="col-4">
+        <bib-datepicker v-model="startDateInput" :value="startDateInput" format="dd MMM yyyy" label="Start date" placeholder="Start date" @input="debounceUpdateField('Start date', 'startDate', startDateInput)"></bib-datepicker>
+      </div>
+      <div class="col-4">
+        <bib-datepicker class="align-right" v-model="dueDateInput" :value="dueDateInput" format="dd MMM yyyy" label="Due date" placeholder="Due date" @input="debounceUpdateField('Due date', 'dueDate', dueDateInput)"></bib-datepicker>
+      </div>
+    </div>
+    <div class="row mx-0" id="sidebar-row-2">
+      <div class="col-4" id="sidebar-col-2">
+        <bib-input type="select" label="Project" :options="companyProjects" v-model.number="form.projectId" v-on:change.native="changeProject"></bib-input>
+      </div>
+      <div class="col-4">
+        <bib-input type="select" label="Section" :options="sectionOpts" v-model.number="form.sectionId" placeholder="Please select ..." v-on:change.native="debounceUpdateField('Section', 'sectionId', form.sectionId)" :disabled="!form.projectId"></bib-input>
+      </div>
+      <div class="col-4" id="sidebar-col-3">
+        <bib-input type="select" label="Department" :options="department" placeholder="Please select..."></bib-input>
+      </div>
+    </div>
+    <div class="row mx-0" id="sidebar-row-3">
+      <div class="col-4" id="sidebar-col-4">
+        <bib-input type="select" label="Priority" v-model.number="form.priorityId" :options="priorityValues" placeholder="Please select..." v-on:change.native="debounceUpdateField('Priority', 'priorityId', form.priorityId)"></bib-input>
+      </div>
+      <div class="col-4" id="sidebar-col-5">
+        <bib-input type="select" label="Status" v-model.number="form.statusId" :options="statusValues" placeholder="Please select..." v-on:change.native="debounceUpdateField('Status', 'statusId', form.statusId)"></bib-input>
+      </div>
+    </div>
+    <div class="row mx-0" id="sidebar-row-4">
+      <div class="col-12" id="sidebar-col-6">
+        <bib-input type="textarea" v-model.trim="form.description" placeholder="Enter task description..." label="Description" v-on:keyup.native="debounceUpdateField('Description', 'description', form.description)"></bib-input>
+      </div>
+    </div>
+    <!-- <div class="py-05 px-105" id="sidebar-btn-wrapper">
+      <bib-button v-show="!currentTask.id" label="Create Task" variant="primary" v-on:click="createTask"></bib-button>
+    </div> -->
+    <loading :loading="loading"></loading>
+  </div>
+</template>
+<script>
+import { DEPARTMENT, STATUS, PRIORITY } from '~/config/constants.js'
+import { mapGetters } from "vuex"
+import dayjs from 'dayjs'
+import _ from 'lodash'
+export default {
+
+  name: 'SidebarFields',
+
+  data() {
+    return {
+      assignee: "",
+      statusValues: STATUS,
+      priorityValues: PRIORITY,
+      department: DEPARTMENT,
+      form: {
+
+      },
+      loading: false,
+    }
+  },
+  computed: {
+    ...mapGetters({
+      teamMembers: "user/getTeamMembers",
+      sections: "section/getProjectSections",
+      currentTask: 'task/getSelectedTask',
+      project: "project/getSingleProject",
+      projects: "project/getAllProjects",
+    }),
+    orgUsers() {
+      let data = this.teamMembers
+        .map(u => {
+          return { label: u.firstName + ' ' + u.lastName, img: u.avatar, value: u.id }
+        })
+      return [{ label: 'Please select...', value: null }, ...data]
+    },
+    companyProjects() {
+      // console.log("new project", this.project.id, this.project.title)
+      let data = this.projects.map(p => {
+        return { label: p.title, value: p.id }
+      })
+      return [{ label: 'Please select...', value: null }, ...data]
+    },
+    sectionOpts() {
+      let sec = [{ label: "Select section", value: "_section" + this.form.projectId }]
+      this.sections.forEach((s) => {
+        if (s.title.includes("_section")) {
+          return false
+        }
+        sec.push({ label: s.title, value: s.id })
+      });
+      return sec
+    },
+    startDateInput: {
+      get() {
+        if (!this.form.startDate) {
+          return new Date()
+        } else {
+          return new Date(this.form.startDate)
+        }
+      },
+      set(newValue) {
+        this.form.startDate = new Date(newValue)
+      }
+    },
+    dueDateInput: {
+      get() {
+        let nd
+        if (!this.form.dueDate) {
+          nd = new Date()
+          return new Date()
+        } else {
+          nd = new Date(this.form.dueDate)
+          return new Date(this.form.dueDate)
+        }
+      },
+      set(newValue) {
+        this.form.dueDate = new Date(newValue)
+      }
+    },
+  },
+  watch: {
+    currentTask() {
+      // console.log(this.currentTask)
+      if (Object.keys(this.currentTask).length) {
+        // this.form = JSON.parse(JSON.stringify(this.currentTask));
+        this.form = _.cloneDeep(this.currentTask);
+        if (this.currentTask.project?.length) {
+          this.form.projectId = this.currentTask.project[0].projectId || this.currentTask.project[0].project.id
+        } else {
+          this.form.projectId = this.project.id
+        }
+        // console.info(this.$refs.topScroll)
+      } else {
+        this.form = {
+          id: '',
+          title: "Enter title...",
+          createdAt: "",
+          startDate: "",
+          dueDate: "",
+          userId: "",
+          sectionId: "_section" + this.project.id,
+          projectId: this.project.id,
+          statusId: 1,
+          priorityId: 2,
+          description: '',
+          budget: 0,
+        }
+        if (this.sectionIdActive) {
+          this.form.sectionId = this.sectionIdActive
+        }
+      }
+    },
+  },
+  methods: {
+    changeProject() {
+      if (!this.form.projectId || this.form.projectId == "") {
+        this.form.projectId = null
+        this.form.sectionId = null
+        if (this.form.id) {
+          // this.updateTask('removed from project', this.form.projectId)
+          this.debounceUpdateField("Project", "projectId", this.form.projectId)
+          // this.$emit("update-field", { name: "Project", field: 'projectId', value: this.form.projectId })
+          return false
+        }
+        return false
+      }
+      this.loading = true
+      if (this.form.projectId && (!this.form.sectionId || this.form.sectionId == "")) {
+        this.form.sectionId = "_section" + this.form.projectId
+      }
+      // console.log(this.form, this.form.projectId)
+      this.$store.dispatch("section/fetchProjectSections", { projectId: this.form.projectId, filter: 'all' }).then((sections) => {
+        // console.log(sections)
+        if (!this.form.id || this.form.id == "") {
+          this.loading = false
+          return false
+        }
+        let sec = sections.find(s => s.title.includes("_section"))
+        let proj = this.companyProjects.find(p => p.value == this.form.projectId)
+        // console.warn(sec);
+        if (!sec) {
+          // this.form.sectionId = null
+          this.form.sectionId = "_section" + this.form.projectId
+        } else {
+          this.form.sectionId = sec.id
+        }
+        this.loading = false
+        // this.updateTask(`removed from project "${proj.label}"`, this.form.projectId)
+        this.debounceUpdateField("Project", "projectId", this.form.projectId)
+        // this.$emit("update-field", { name: "Project", field: "projectId", value: this.form.projectId })
+
+      })
+    },
+    debounceUpdateField: _.debounce(function(name, field, value) {
+    // updateField(name, field, value) {
+      console.log(name, field, value)
+      this.$emit("update-field", { name: name, field: field, value: value })
+    }, 1000)
+  }
+}
+
+</script>
+<style lang="scss" scoped>
+</style>
