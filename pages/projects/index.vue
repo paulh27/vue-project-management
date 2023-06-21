07aaa81a9f2b55c3a430c3dfca932bd0e33687c7
CodeMastah <paul@project-x.site>
2023-06-21 19:47:16 +0530

    table new row and other fixes

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -29,7 +29,7 @@
         </template>
 
         <template v-else>
-          <advance-table :tableFields="tableFields" :tableData="localData" :contextItems="projectContextItems" @context-item-event="contextItemClick" @row-click ="projectRoute" @context-open="contextOpen" @table-sort="sortProject" @title-click="projectRoute" @update-field="updateProject" @create-row="createProject" sectionTitle="" :newTaskButton="{label: 'New Project', icon: 'add'}" :drag="false"></advance-table>
+          <advance-table :tableFields="tableFields" :tableData="localData" :contextItems="projectContextItems" @context-item-event="contextItemClick" @row-click ="projectRoute" @context-open="contextOpen" @table-sort="sortProject" @title-click="projectRoute" @update-field="updateProject" @create-row="createProject" sectionTitle="" :plusButton="{label: 'New Project', icon: 'add'}" :drag="false"></advance-table>
         </template> 
 
       </template>
