b3a88a764adb4677ac624f204005b5c08a2832cc
CodeMastah <paul@project-x.site>
2023-07-20 17:13:32 +0530

    added cookie temporary while checking possible solution

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -113,10 +113,11 @@ export default {
 
   asyncData(context){
       // console.log(context.store.state.token)
-    // const token = context.$cookies.get('b_ssojwt')
+      // const token = context.$cookies.get('b_ssojwt')
+      const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJrNjFZUWRKNko3bGRPR3BKIiwic3ViZSI6ImRocnV2LnNoYXJtYUBxc3N0ZWNobm9zb2Z0LmNvbSIsInN1YnMiOiJBQ1RJVkUiLCJzdWJiIjoiTzNHV3BtYms1ZXpKbjRLUiIsInN1YmJzIjoiQ0xJRU5UIiwic3ViciI6IkFETUlOIiwic3ViYyI6IkNhbmFkYSIsImVudiI6ImRldiIsImlhdCI6MTY4OTg1MDM0ODYxMCwiZXhwIjoxNjk3NjI2MzQ4NjEwLCJqdGkiOiIxYWI4MDVlMC0zYTkyLTQxNDMtYmMyOC0zNGM2ZmRhZGFkZDgifQ.5-G-YJ16WfrZBp5VhK_p2-qULAP9jpF5ZOqsQ7Phs_0"
       return context.$axios.$get(`project/company/all`, {
       headers: {
-        'Authorization': `Bearer ${context.store.state.token}`,
+        'Authorization': `Bearer ${token}`,
         'Filter': 'all'
       }
     }).then((res)=>{
