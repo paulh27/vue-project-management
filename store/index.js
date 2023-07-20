034e8eb9a97d1d904b927df072454c8a7fb3380e
CodeMastah <paul@project-x.site>
2023-07-20 16:48:34 +0530

    updated the code for SSR token error

--- store/index.js
+++ store/index.js
@@ -1,11 +1,28 @@
 export const state = () => ({
-
+    token: null
 })
 
 export const mutations = {
-
+    setToken(state, payload) {
+        state.token = payload;
+    }
 }
+
+export const getters = {
+    getToken(state) {
+        return state.token;
+    }
+  };
 
 export const actions = {
+    nuxtServerInit ({ commit }, { store, route, req, res, app, beforeNuxtRender }) {
+        
+        let token = app.$cookies.get('b_ssojwt')
+        commit('setToken', token);
+        // console.log("In Nuxt SERVER INIT >>>>>",token) 
 
-}
+        //   beforeNuxtRender(({ nuxtState }) => {
+            
+        //   })
+      }
+  }
