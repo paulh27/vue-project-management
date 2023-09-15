b4c3e05731906dcc24ce9eba84702821a4bb2b66
CodeMastah <paul@project-x.site>
2023-09-15 05:34:38 -0400

    scroll update

--- components/organism/AdvTableThree.vue
+++ components/organism/AdvTableThree.vue
@@ -425,7 +425,9 @@ export default {
       if (this.allDataDisplayed) {
         return; // Stop adding data if all data has been displayed
       }
-      const isAtBottom = tableContainer.scrollTop + tableContainer.clientHeight+5 >= tableContainer.scrollHeight;
+      const scrollThreshold = tableContainer.scrollHeight * 0.6; // 60% of the scroll height
+      const isAtBottom = tableContainer.scrollTop + tableContainer.clientHeight + 5 >= scrollThreshold;
+      // const isAtBottom = tableContainer.scrollTop + tableContainer.clientHeight+5 >= tableContainer.scrollHeight;
 
       if (isAtBottom) {
                 this.showData();
