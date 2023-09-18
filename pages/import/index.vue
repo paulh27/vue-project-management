42dd1ec19005d5412e26b1f01883b50bbd7ac032
CodeMastah <paul@project-x.site>
2023-09-18 19:26:29 +0530

    ui for import

--- pages/import/index.vue
+++ pages/import/index.vue
@@ -1,31 +1,62 @@
 <template>
     <client-only>
-        <div id="page" class="project-id-wrapper ">
+        <div id="page" class="project-id-wrapper h-100 overflow-y-auto">
             <page-title title="Import"></page-title>
             <!-- <task-actions :gridType="gridType" @change-grid-type="changeGridType" @create-task="openSidebar" @add-section="toggleNewsection"></task-actions> -->
             <!-- Task View -->
-            <div id="project-id-content" class="project-id-content position-relative overflow-y-auto" :style="{ 'width': contentWidth }">
+            <div id="project-id-content" class="project-id-content position-relative " :style="{ 'width': contentWidth }">
                 <div class="d-flex justify-center p-1">
-                  <div class="border-gray2 shape-rounded py-1" style="max-width:300px;">
-                    <bib-input type="file" ref="csvImport"  @files-dropped="onFileInput" variant="accepted" iconLeft="upload" placeholder="Drop file here or click to upload"></bib-input>
-                    <bib-button variant="secondary" label="Import CSV" @click="onsubmit"></bib-button>
-                  </div>
+                    <div class="border-gray2 shape-rounded py-1" style="min-width: 300px; max-width:400px;">
+                        <bib-input type="file" ref="csvImport" @files-dropped="onFileInput" multiple="false" variant="accepted" iconLeft="upload" placeholder="Click to upload"></bib-input>
+                        <div class="mx-1 mt-1 align-center">
+                          <bib-button :disabled="loading" variant="secondary" label="Import CSV" @click="onsubmit"></bib-button> <bib-spinner v-if="loading" :scale="2" ></bib-spinner>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-            <div v-if="appMembers.length > 0" class="col-6">
-              <h3>Users in Central Biztree</h3>
-              <ol>
-                <li v-for="(user, index) in appMembers">{{user.email}}</li>
-              </ol>
-            </div>
-            <div v-if="userList.length > 0" class="col-6">
-              <h3>Users in Asana Project</h3>
-              <ol>
-                <li v-for="(user, index) in userList">{{user}}</li>
-              </ol>
-            </div>
-            </div>
+            <!-- <div class="row">
+                <div v-if="appMembers.length > 0" class="col-4">
+                    <h3>Users in Central Biztree</h3>
+                    <ol>
+                        <li v-for="(user, index) in appMembers">{{user.label}} ({{user.email}})</li>
+                    </ol>
+                </div>
+                <div v-if="userList.length > 0" class="col-4">
+                    <h3>Users from Asana Project</h3>
+                    <ol>
+                        <li v-for="(user, index) in userList">{{user}}</li>
+                    </ol>
+                </div>
+                <div v-if="missingMembers.length > 0" class="col-4">
+                    <h3>Missing members from import</h3>
+                    <ol>
+                        <li v-for="(mm, index) in missingMembers">{{mm}}</li>
+                    </ol>
+                </div>
+            </div> -->
+            <!-- modal -->
+            <bib-modal-wrapper v-if="importmodal" title="Import result" @close="closeModal">
+              <!-- <template slot="header">
+                
+              </template> -->
+                <template slot="content">
+                    <div v-show="missingMembers.length > 0" class="of-scroll-y  " style="max-height: 400px">
+                        <!-- <p v-for="(mm, index) in missingMembers">{{index}}. {{mm.label}} - {{(mm.email)}}</p> -->
+                        <h4>Missing member from import</h4>
+                        <p v-for="(mm, index) in missingMembers"> {{index+1}}. {{mm}}</p>
+                    </div>
+                    <div v-show="importfinish" class="shape-rounded align-center gap-05 border-success text-success p-05">
+                      <bib-icon icon="tick" variant="success"></bib-icon>
+                      Import finished successfully
+                    </div>
+                </template>
+                <template slot="footer">
+                    <div v-show="!importfinish" class="justify-between">
+                        <bib-button label="Skip" variant="secondary" class="mr-1" pill @click="closeModal"></bib-button>
+                        <bib-button label="Continue" variant="primary" pill @click="importData"></bib-button>
+                    </div>
+                </template>
+            </bib-modal-wrapper>
             <alert-dialog v-show="alertDialog" :message="alertMsg" @close="alertDialog = false"></alert-dialog>
             <!-- notification -->
             <bib-popup-notification-wrapper>
@@ -37,7 +68,6 @@
         </div>
     </client-only>
 </template>
-
 <script>
 import { mapGetters } from 'vuex'
 import _ from 'lodash'
@@ -66,8 +96,9 @@ export default {
                 icon: "add",
             },
 
-            userList: []
-
+            userList: [],
+            importmodal: false,
+            missingMembers: [],
         }
     },
 
@@ -108,23 +139,36 @@ export default {
 
     methods: {
         async onFileInput(file) {
-            // console.log(file) 
+          console.log(file) 
         },
 
         async onsubmit() {
-          let file = this.$refs.csvImport.filesUploaded;
+          this.loading = true
+            let file = this.$refs.csvImport.filesUploaded;
 
-          let formdata = new FormData();
-          formdata.append('file', file[0])
+            let formdata = new FormData();
+            formdata.append('file', file[0])
 
-          let users = await this.$axios.post("/file/import", formdata, {
-              headers: {
-                'Content-Type': 'multipart/form-data',
-                'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
-              }
+            let users = await this.$axios.post("/file/import", formdata, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'Authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
             })
+            console.log(users.data.data, this.appMembers)
+            if (users.data.statusCode == 200) {
+              let appMemberEmails = this.appMembers.map(member => member.email); 
+              this.missingMembers = users.data.data.filter(email => !appMemberEmails.includes(email)); 
 
-          this.userList = await users?.data?.data;
+              this.importmodal = true
+              console.log(this.missingMembers)
+              this.loading = false
+            } else {
+              this.popupMessages.push({text: "Some error occured", variant: "danger"})
+              this.loading = false
+            }
+
+            this.userList = await users?.data?.data;
         },
 
         /*async uploadFile(commentFiles, data){
@@ -150,6 +194,17 @@ export default {
             this.$nuxt.$emit("get-msg-files")
           }
         }*/
+        closeModal(){
+          this.importmodal = false
+          this.missingMembers = []
+          this.importfinish = false
+        },
+        importData(){
+          // _.delay(function() {
+            this.missingMembers = []
+            this.importfinish = true
+          // }, 1200)
+        },
 
     }
 }
