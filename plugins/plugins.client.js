28cc7007b491147c0ee8b3b196eb95444f407612
CodeMastah <paul@project-x.site>
2023-11-23 11:21:10 +0530

    library update

--- plugins/plugins.client.js
+++ plugins/plugins.client.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import vClickOutside from 'v-click-outside'
-import BibUILibrary from "~/node_modules/@bizinbox/bib-ui-lib/dist/library";
+// import BibUILibrary from "~/node_modules/@bizinbox/bib-ui-lib/dist/library";
+import BibUILibrary from '@bizinbox/bib-ui-lib'
 
-Vue.use(BibUILibrary)
+// Vue.use(BibUILibrary)
 
 Vue.use(Vuelidate)
 Vue.use(vClickOutside)
+
+export default ({ store }, inject) => {
+    Vue.use(BibUILibrary, { store })
+}
